6ed6f30ab0421054c727d5b865a79cd36f384f86
Erick Christian <erickchristian48@gmail.com>
2025-07-07 14:53:55 +0700

    feat: setup query client

--- src/app.tsx
+++ src/app.tsx
@@ -1,39 +1,38 @@
-import { useState } from "react"
+import type { ReactNode } from "react"
 
-import reactLogo from "./assets/react.svg"
+import { QueryClientProvider } from "@tanstack/react-query"
 
-import viteLogo from "/vite.svg"
-import "./App.css"
+import { queryClient } from "./lib/query"
 
 export function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Providers>
+      <div className="mx-auto w-[min(calc(100%-2rem)_,400px)] bg-fuchsia-500">
+        <form>
+          <input
+            className="w-full rounded-lg border border-gray-300 bg-white px-4 py-2 text-gray-900 placeholder-gray-400 transition duration-150 ease-in-out focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:placeholder-gray-500"
+            name="username"
+            type="text"
+          />
+          <button type="submit"></button>
+
+          <p>Showing</p>
+
+          <Spinner />
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          onClick={() => {
-            setCount((count) => count + 1)
-          }}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </Providers>
   )
 }
+
+const Providers = (props: { children?: ReactNode }) => (
+  <QueryClientProvider client={queryClient}>
+    {props.children}
+  </QueryClientProvider>
+)
+
+const Spinner = () => (
+  <div className="flex items-center justify-center">
+    <div className="size-8 animate-spin rounded-full border-b border-t border-blue-500"></div>
+  </div>
+)
