81da63db127f042af21231d59c4b0dc2808fe2bb
Erick Christian <erickchristian48@gmail.com>
2025-07-07 14:53:18 +0700

    setup tailwind correctly

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,19 @@
 import config from "@echristian/eslint-config"
 
 export default config({
+  jsx: {
+    enabled: true,
+    a11y: true,
+  },
   react: {
     enabled: true,
   },
   reactHooks: {
     enabled: true,
+  },
+
+  prettier: {
+    tailwindStylesheet: "./src/styles/global.css",
+    plugins: ["prettier-plugin-tailwindcss"],
   },
 })
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client"
 import invariant from "tiny-invariant"
 
 import { App } from "./app.tsx"
+// Styling
+import "./styles/global.css"
 
 const rootElement = document.querySelector("#root")
 invariant(rootElement, "Root element not found")
