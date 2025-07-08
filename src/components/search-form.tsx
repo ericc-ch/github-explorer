bc3fa57cea7336630f0b9f908715e2ece8ff87c1
Erick Christian <erickchristian48@gmail.com>
2025-07-08 15:05:17 +0700

    style: style form

--- src/components/search-form.tsx
+++ src/components/search-form.tsx
@@ -7,14 +7,14 @@ export function SearchForm(props: SearchFormProps) {
     <form className="flex flex-col gap-2" {...props}>
       <input
         required
-        className="w-full border border-gray-300 bg-gray-100 px-4 py-2 text-gray-900 placeholder-gray-400 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500"
+        className="w-full rounded-lg border border-gray-300 bg-white px-4 py-2 text-gray-900 placeholder-gray-400 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500"
         disabled={props.disabled}
         name="username"
         placeholder="Enter username"
         type="text"
       />
       <button
-        className="transform cursor-pointer bg-blue-500 px-4 py-2 font-bold text-white duration-200 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-1 disabled:cursor-not-allowed disabled:bg-gray-400"
+        className="transform cursor-pointer rounded-lg bg-blue-500 px-4 py-2 font-bold text-white shadow-sm duration-200 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-gray-400"
         disabled={props.disabled}
         type="submit"
       >
